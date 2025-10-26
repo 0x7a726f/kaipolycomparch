87257c38b21e9c32768953f68e1c14544b3f25a9
0x7a726f <69540160+0x7a726f@users.noreply.github.com>
2025-10-26 20:41:32 +0200

    Refactor MIPS assembly for min function and loop

--- prog1.s
+++ prog1.s
@@ -1,23 +1,42 @@
+#===============================================================
 #1A
+#Με int* array = $a0, int size = $a1 και $v0 = min
+.data
+array: .word 6, 1, 2, 8, 4      #  array
+size:  .word 5                  #  size
 
-#a0 = *array , $a1 = size , $ν0 = max 
+.text
+.globl main                     # entry point
+
+main:
+    la   $a0, array             # $a0 = &array[0]
+    lw   $a1, size              # $a1 = size (5)
+    jal  min                    # call min(array, size)
+    
+    move $a0, $v0               # Για εκτύπωση
+    li   $v0, 1                 # syscall: print_int
+    syscall
+
+    li   $v0, 10                # syscall: exit
+    syscall
+
 min:
-    lw      $t0, 0($a0)        # $t0 = array[0] Εδώ αποθηκεύω το minimum, το αρχικοποιώ στο πρώτο στοιχείο του array
+    lw      $t0, 0($a0)         # $t0 = array[0] Εδώ αποθηκεύω το minimum, το αρχικοποιώ στο πρώτο στοιχείο του array
+    move    $v0, $t0            # min = array[0]
     addi    $t1, $zero, 1       # $t1 = 1 (loop μεταβλητή i=1)
 loop:
     bge     $t1, $a1, done      # if i >= size, exit loop
     sll     $t2, $t1, 2         # $t2 = i * 4 (byte offset επειδή πιάνουν 4 θέσεις)
-    add     $t2, $a0, $t2       # $t2 δείχνει στην αρχή του array[i]
-    lw      $t3, 0($t2)         # $t3 = array[i] Για να αποθηκεύσω τη τιμή στο t3
-    blt     $t3, $t0, update    # if array[i] < minimum, update
+    add     $t3, $a0, $t2       # $t3 δείχνει στην αρχή του array[i]
+    lw      $t4, 0($t3)         # $t4 = array[i] Για να αποθηκεύσω τη τιμή στο t4
+    blt     $t4, $v0, update    # if array[i] < minimum, update
     addi    $t1, $t1, 1         # i++
     j       loop
 update:
-    move    $t0, $t3            # minimum = array[i]
+    move    $v0, $t4            # minimum = array[i]
     addi    $t1, $t1, 1         # i++
     j       loop
 done:
-    move    $v0, $t0            # return minimum
     jr      $ra
 
 #===============================================================
